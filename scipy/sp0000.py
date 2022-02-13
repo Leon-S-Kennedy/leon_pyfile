7928e04da7d35d1008e5a4bb54fb18a5b1e3c149
Leon-S-Kennedy <937854328@qq.com>
2022-02-13 17:53:38 +0800

    基本函数

---
+++ scipy/sp0000.py
@@ -0,0 +1,10 @@
+import numpy as np
+from scipy import special
+i=special.cbrt([[1,27],[8,1000]])
+print(i)
+print('----------------------------------------')
+j=special.gamma(10)
+print(j)
+print('-------------------------------------------')
+k=special.erf(1)
+print(k)
